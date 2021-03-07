b653470b3b0d80a1065c75490c5d269762bffa5f
waxx <60652127+872119925@users.noreply.github.com>
2021-03-08 05:18:23 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -23,6 +23,7 @@ def readSecret(self, key, default=""):
 #push推送
 def pushPlusNotify():
     receivers = readSecret('PUSH_PLUS_TOKEN')
+    if receivers != '':
     content = readFile('log.txt')
     response = json.dumps(requests.post('https://pushplus.hxtrip.com/send', data={
                           'token': receivers, 'title':'UnicomTask每日报表', 'content': content, 'topic': ''}, headers={'Content-Type': 'application/json;charset=utf-8'}).json(), ensure_ascii=False)
